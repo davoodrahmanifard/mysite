6bfc6557d4b84a00af063beaf36f5d8889fa3d52
rahmanifard <42175149+rahmanifard@users.noreply.github.com>
2021-11-09 14:34:32 +0100

    Add files via upload

---
+++ src/pages/about.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def main():
+    st.title("About")
+    st.info(
+        """
+        This app is maintained by Davood Rahmanifard. You can learn more about me at
+        [Davood Github](https://github.com/rahmanifard/).
+        """
+)
---
+++ src/pages/contribute.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+
+def main():
+    st.title("Contribute")
+    st.info("Feel free to contribute to this open source project by sending email to me: "
+                    "[Email](davood.rahmnaifard@gmail.com)")
